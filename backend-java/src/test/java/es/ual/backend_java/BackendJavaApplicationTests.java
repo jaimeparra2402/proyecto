9e38be750e426f4e6ea001ebcb124535a1f6052d
bete2043 <jaimillo979@gmail.com>
2026-05-21 18:32:43 +0200

    feat: initial monorepo structure with Ionic, Node, and Java skeletons

---
+++ backend-java/src/test/java/es/ual/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ual.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
