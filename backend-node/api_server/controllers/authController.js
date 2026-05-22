3b8e738df65d60fdfad86fc820a49a3e942b4e0e
bete2043 <jaimillo979@gmail.com>
2026-05-23 00:01:18 +0200

    Fix (Node): Fixed action error

--- backend-node/api_server/controllers/authController.js
+++ backend-node/api_server/controllers/authController.js
@@ -82,7 +82,7 @@ exports.protect = async (req, res, next) => {
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ status: 'fail', message: 'Permission denied' });
+      return res.status(403).json({ status: 'fail', message: 'Permission denied'});
     }
     next();
   };
